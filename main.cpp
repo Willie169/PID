fb0ea785904026142c4acd2704d085d6274a615b
Willie169 <cg10250207@gmail.com>
2024-11-05 23:01:36 +0800

    k

--- main.cpp
+++ main.cpp
@@ -2,17 +2,17 @@
 
 int main() {
     vector<ParameterRange> ranges = {
-        {100, 1000, 100},    // maxIntTm
-        {10, 100, 10},     // maxAmp
-        {0, 1, 0.1},       // minKp
-        {0.1, 1, 0.1},      // maxKp
-        {1, 10, 1},       // rTiM
-        {1, 10, 1},       // TdM
-        {1, 10, 1},        // TddM
-        {100, 1000, 100}, // eDPm
-        {0, 0.1, 0.01},    // eDPa
+        {500, 500, 0},    // maxIntTm
+        {50, 50, 0},     // maxAmp
+        {0.01, 0.01, 0},       // minKp
+        {0.1, 0.5, 0.1},      // maxKp
+        {10, 10, 0},       // rTiM
+        {10, 10, 0},       // TdM
+        {1, 1, 0},        // TddM
+        {300, 300, 0}, // eDPm
+        {0.01, 0.01, 0},    // eDPa
         {0, 150, 10},       // session
-        {0, 0.5, 0.01}      // Kp
+        {0.01, 0.5, 0.01}      // Kp
     };
     
     return optimize(ranges);
