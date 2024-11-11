5b9e95caf17b302710d1b6aa133b411b72a05019
Willie Shen <cg10250207@gmail.com>
2024-11-11 14:43:25 +0800

    j

--- main.ino
+++ main.ino
@@ -1,23 +1,44 @@
 #include "PID.hpp"
-#define TARGET 10
-#define MULTIPLIER 0.5
+#define TARGET_DISTANCE 10
+#define AVERAGE_VELOCITY_MULTIPLIER 0.5
+#define DIFFERENTIAL_VELOCITY_MULTIPLIER 0.5
+#define DISTANCE_BETWEEN_ULTRASONIC 5
 
-PID pid(461.9, 517.9, 0.00265, 0.472, 1, 65.37, 1.7, 125, 0.15, 140, 0.2436);
-double out;
+PID avgPID(461.9, 517.9, 0.00265, 0.472, 1, 65.37, 1.7, 125, 0.15, 140, 0.2436);
+PID difPID(461.9, 517.9, 0.00265, 0.472, 1, 65.37, 1.7, 125, 0.15, 140, 0.2436);
+double avgV;
+double difV;
 
-double input() {
+inline double leftIn() {
+    return 0;
+}
+
+inline double rightIn() {
     return 0;
 }
 
 void setup() {
-    out = 0;
+    avgV = 0;
+    difV = 0;
 }
 
 void loop() {
-    out += pid.update((input() - TARGET), millis()) * MULTIPLIER;
-    output(out);
+    double left = leftIn();
+    double right = rightIn();
+    double avg = left + right;
+    double dif = atan2(left - right, DISTANCE_BETWEEN_ULTRASONIC);
+    
+    avgV += avgPID.update((avg - TARGET_DISTANCE), millis()) * AVERAGE_VELOCITY_MULTIPLIER;
+    difV += difPID.update((dif - TARGET_DISTANCE), millis()) * DIFFERENTIAL_VELOCITY_MULTIPLIER;
+    
+    leftOut(avgV + difV);
+    rightOut(avgV - difV);
 }
 
-inline void output(double output) {
-    Serial.println(output);
+inline void leftOut(double output) {
+    Serial.println("Left: " + String(output));
+}
+
+inline void rightOut(double output) {
+    Serial.println("Right: " + String(output));
 }
