260b9dd400762415b14592f435316ec89ee4ac29
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-11-25 19:51:43 +0800

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,8 +1,9 @@
 #include "PID.hpp"
 #define TARGET_DISTANCE 10
-#define AVERAGE_VELOCITY_MULTIPLIER 0.5
-#define DIFFERENTIAL_VELOCITY_MULTIPLIER 0.5
 #define DISTANCE_BETWEEN_ULTRASONIC 5
+#define DIFFERENTIAL_SPEED_MULTIPLIER 0.8
+#define POSITIVE_SPEED_MULTIPLIER 1
+#define NEGATIVE_SPEED_MULTIPLIER 1.2
 
 PID avgPID(461.9, 517.9, 0.00265, 0.472, 1, 65.37, 1.7, 125, 0.15, 140, 0.2436);
 PID difPID(461.9, 517.9, 0.00265, 0.472, 1, 65.37, 1.7, 125, 0.15, 140, 0.2436);
@@ -28,11 +29,15 @@ void loop() {
     double avg = left + right;
     double dif = atan2(left - right, DISTANCE_BETWEEN_ULTRASONIC);
     
-    avgV += avgPID.update((avg - TARGET_DISTANCE), millis()) * AVERAGE_VELOCITY_MULTIPLIER;
-    difV += difPID.update((dif - TARGET_DISTANCE), millis()) * DIFFERENTIAL_VELOCITY_MULTIPLIER;
+    avgV += avgPID.update((avg - TARGET_DISTANCE), millis());
+    difV += difPID.update((dif - TARGET_DISTANCE), millis()) * DIFFERENTIAL_SPEED_MULTIPLIER;
     
-    leftOut(avgV + difV);
-    rightOut(avgV - difV);
+    double leftV = avgV + difV;
+    leftV *= (leftV>0) ? POSITIVE_SPEED_MULTIPLIER : NEGATIVE_SPEED_MULTIPLIER;
+    double rightV = avgV - difV;
+    rightV *= (rightV>0) ? POSITIVE_SPEED_MULTIPLIER : NEGATIVE_SPEED_MULTIPLIER;
+    leftOut();
+    rightOut();
 }
 
 inline void leftOut(double output) {
